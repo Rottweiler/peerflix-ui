85047d71e9efb4e06e6c9dfef9b32000d25b0291
Rottweiler <rottweiler.hackhound@aol.com>
2015-11-12 02:39:02 +0100

    first commit

---
+++ peerflix-ui/Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits MaterialSkin.Controls.MaterialForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.SearchTextBox = New MaterialSkin.Controls.MaterialSingleLineTextField()
+        Me.SearchResults = New System.Windows.Forms.ListBox()
+        Me.SearchButton = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.VideoPlayer = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.MaterialTabSelector1 = New MaterialSkin.Controls.MaterialTabSelector()
+        Me.MaterialTabControl1 = New MaterialSkin.Controls.MaterialTabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.StatusLabel = New MaterialSkin.Controls.MaterialLabel()
+        CType(Me.VideoPlayer, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MaterialTabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SearchTextBox
+        '
+        Me.SearchTextBox.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SearchTextBox.Depth = 0
+        Me.SearchTextBox.Hint = ""
+        Me.SearchTextBox.Location = New System.Drawing.Point(448, 6)
+        Me.SearchTextBox.MouseState = MaterialSkin.MouseState.HOVER
+        Me.SearchTextBox.Name = "SearchTextBox"
+        Me.SearchTextBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.SearchTextBox.SelectedText = ""
+        Me.SearchTextBox.SelectionLength = 0
+        Me.SearchTextBox.SelectionStart = 0
+        Me.SearchTextBox.Size = New System.Drawing.Size(267, 23)
+        Me.SearchTextBox.TabIndex = 1
+        Me.SearchTextBox.Text = "Search term, keywords"
+        Me.SearchTextBox.UseSystemPasswordChar = False
+        '
+        'SearchResults
+        '
+        Me.SearchResults.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SearchResults.FormattingEnabled = True
+        Me.SearchResults.Location = New System.Drawing.Point(448, 35)
+        Me.SearchResults.Name = "SearchResults"
+        Me.SearchResults.Size = New System.Drawing.Size(389, 290)
+        Me.SearchResults.TabIndex = 2
+        '
+        'SearchButton
+        '
+        Me.SearchButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SearchButton.Depth = 0
+        Me.SearchButton.Location = New System.Drawing.Point(721, 6)
+        Me.SearchButton.MouseState = MaterialSkin.MouseState.HOVER
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Primary = True
+        Me.SearchButton.Size = New System.Drawing.Size(116, 23)
+        Me.SearchButton.TabIndex = 4
+        Me.SearchButton.Text = "Search"
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
+        'VideoPlayer
+        '
+        Me.VideoPlayer.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.VideoPlayer.Enabled = True
+        Me.VideoPlayer.Location = New System.Drawing.Point(3, 6)
+        Me.VideoPlayer.Name = "VideoPlayer"
+        Me.VideoPlayer.OcxState = CType(resources.GetObject("VideoPlayer.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.VideoPlayer.Size = New System.Drawing.Size(439, 325)
+        Me.VideoPlayer.TabIndex = 0
+        '
+        'MaterialTabSelector1
+        '
+        Me.MaterialTabSelector1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.MaterialTabSelector1.BaseTabControl = Me.MaterialTabControl1
+        Me.MaterialTabSelector1.Depth = 0
+        Me.MaterialTabSelector1.Location = New System.Drawing.Point(0, 64)
+        Me.MaterialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialTabSelector1.Name = "MaterialTabSelector1"
+        Me.MaterialTabSelector1.Size = New System.Drawing.Size(851, 23)
+        Me.MaterialTabSelector1.TabIndex = 5
+        Me.MaterialTabSelector1.Text = "MaterialTabSelector1"
+        '
+        'MaterialTabControl1
+        '
+        Me.MaterialTabControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.MaterialTabControl1.Controls.Add(Me.TabPage1)
+        Me.MaterialTabControl1.Controls.Add(Me.TabPage2)
+        Me.MaterialTabControl1.Depth = 0
+        Me.MaterialTabControl1.Location = New System.Drawing.Point(0, 89)
+        Me.MaterialTabControl1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialTabControl1.Name = "MaterialTabControl1"
+        Me.MaterialTabControl1.SelectedIndex = 0
+        Me.MaterialTabControl1.Size = New System.Drawing.Size(851, 366)
+        Me.MaterialTabControl1.TabIndex = 6
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.Color.White
+        Me.TabPage1.Controls.Add(Me.VideoPlayer)
+        Me.TabPage1.Controls.Add(Me.SearchTextBox)
+        Me.TabPage1.Controls.Add(Me.SearchButton)
+        Me.TabPage1.Controls.Add(Me.SearchResults)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(843, 340)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Player"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(843, 325)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Config"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.StatusLabel.AutoSize = True
+        Me.StatusLabel.Depth = 0
+        Me.StatusLabel.Font = New System.Drawing.Font("Roboto", 11.0!)
+        Me.StatusLabel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.StatusLabel.Location = New System.Drawing.Point(12, 459)
+        Me.StatusLabel.MouseState = MaterialSkin.MouseState.HOVER
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Size = New System.Drawing.Size(33, 19)
+        Me.StatusLabel.TabIndex = 7
+        Me.StatusLabel.Text = "Idle"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(851, 487)
+        Me.Controls.Add(Me.StatusLabel)
+        Me.Controls.Add(Me.MaterialTabSelector1)
+        Me.Controls.Add(Me.MaterialTabControl1)
+        Me.Name = "Form1"
+        Me.Text = "peerflix-ui"
+        CType(Me.VideoPlayer, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MaterialTabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents VideoPlayer As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents SearchTextBox As MaterialSkin.Controls.MaterialSingleLineTextField
+    Friend WithEvents SearchResults As ListBox
+    Friend WithEvents SearchButton As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialTabSelector1 As MaterialSkin.Controls.MaterialTabSelector
+    Friend WithEvents MaterialTabControl1 As MaterialSkin.Controls.MaterialTabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents StatusLabel As MaterialSkin.Controls.MaterialLabel
+End Class
---
+++ peerflix-ui/Form1.vb
@@ -0,0 +1,215 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Runtime.InteropServices
+Imports AxWMPLib
+Imports ThePirateBay
+Imports WMPLib
+
+Public Class Form1
+
+    Private ls As IEnumerable(Of Torrent)
+    Private proc As Process
+    'Private iw As StreamWriter
+
+    ''' <summary>
+    ''' Initializers
+    ''' </summary>
+    Sub New()
+        Me.Icon = My.Resources.peerflix_ui_beta
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    ''' <summary>
+    ''' ThePirateBay Search function
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+        StatusLabel.Text = "Searching for " & SearchTextBox.Text
+        ls = Nothing
+        Dim query As New Query(SearchTextBox.Text)
+        Dim torrents As IEnumerable(Of Torrent)
+        Try
+            torrents = Tpb.Search(query)
+        Catch ex As Exception
+            MessageBox.Show("Could not search: " & ex.Message)
+        End Try
+#Disable Warning
+        If torrents IsNot Nothing Then
+            If torrents.Count > 0 Then
+                ls = torrents
+                SearchResults.Items.Clear()
+                For Each torrent As Torrent In torrents
+                    SearchResults.Items.Add(torrent.Name)
+                Next
+            Else
+                StatusLabel.Text = "No search results were found"
+            End If
+        End If
+#Enable Warning
+    End Sub
+
+    ''' <summary>
+    ''' The DoubleClick event of SearchResults ListBox, plays a stream
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub SearchResults_DoubleClick(sender As Object, e As EventArgs) Handles SearchResults.DoubleClick
+        If SearchResults.SelectedIndex <> -1 Then
+            For Each torrent As Torrent In ls
+                If torrent.Name = SearchResults.SelectedItem.ToString Then
+                    StartPeerflix(torrent.Name, torrent.Magnet)
+                End If
+            Next
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Starts the peerflix service, located based on PATH variable
+    ''' </summary>
+    ''' <param name="name"></param>
+    ''' <param name="magnet"></param>
+    Private Sub StartPeerflix(name As String, magnet As String)
+        StopPeerflix()
+        StatusLabel.Text = "Starting playback of " & name
+        Dim psi As New ProcessStartInfo()
+        psi.FileName = "peerflix" 'LocatePeerflix()
+        psi.Arguments = magnet
+        'psi.RedirectStandardInput = True
+        psi.UseShellExecute = True
+        psi.CreateNoWindow = True
+        psi.WindowStyle = ProcessWindowStyle.Hidden
+        proc = Process.Start(psi)
+        'iw = proc.StandardInput
+        VideoPlayer.URL = "http://" & Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(Function(ip) ip.AddressFamily = AddressFamily.InterNetwork).ToString & ":8888/"
+        StatusLabel.Text = "Playing " & name
+    End Sub
+
+    ''' <summary>
+    ''' Stops the peerflix process along with nodejs (process tree)
+    ''' </summary>
+    Private Sub StopPeerflix()
+        If proc IsNot Nothing Then
+            If proc.Handle <> IntPtr.Zero Then
+                If Not proc.HasExited Then
+                    'iw.WriteLine("\x3")
+                    'iw.WriteLine("y")
+                    'iw.Close()
+                    StatusLabel.Text = "Stopping playback.."
+                    Application.DoEvents()
+                    VideoPlayer.Ctlcontrols.stop()
+                    VideoPlayer.URL = ""
+                    VideoPlayer.currentPlaylist.clear()
+                    TerminateProcessTree(proc.Handle, proc.Id, 0)
+                    StatusLabel.Text = "Idle"
+                    'proc.Kill()
+                End If
+            End If
+        End If
+    End Sub
+
+#Region "Kill process tree"
+    '''<summary>
+    ''' Terminate a process tree
+    '''</summary>
+    '''<param name="hProcess">The handle of the process</param>
+    '''<param name="processID">The ID of the process</param>
+    '''<param name="exitCode">The exit code of the process</param>
+    Public Sub TerminateProcessTree(hProcess As IntPtr, processID As UInteger, exitCode As Integer)
+        ' Retrieve all processes on the system
+        Dim processes As Process() = Process.GetProcesses()
+        For Each p As Process In processes
+            ' Get some basic information about the process
+            Dim pbi As New PROCESS_BASIC_INFORMATION()
+            Try
+                Dim bytesWritten As UInteger
+                NtQueryInformationProcess(p.Handle, 0, pbi, CUInt(Marshal.SizeOf(pbi)), bytesWritten)
+                ' == 0 is OK
+                ' Is it a child process of the process we're trying to terminate?
+                If pbi.InheritedFromUniqueProcessId = processID Then
+                    ' The terminate the child process and its child processes
+                    TerminateProcessTree(p.Handle, pbi.UniqueProcessId, exitCode)
+                End If
+            Catch generatedExceptionName As Exception
+                ' Ignore, most likely 'Access Denied'
+                ' ex 
+            End Try
+        Next
+
+        ' Finally, termine the process itself:
+        TerminateProcess(CUInt(hProcess), exitCode)
+    End Sub
+
+    ' [StructLayout(LayoutKind.Sequential)]
+    Private Structure PROCESS_BASIC_INFORMATION
+        Public ExitStatus As Integer
+        Public PebBaseAddress As Integer
+        Public AffinityMask As Integer
+        Public BasePriority As Integer
+        Public UniqueProcessId As UInteger
+        Public InheritedFromUniqueProcessId As UInteger
+    End Structure
+
+    <DllImport("kernel32.dll")>
+    Private Shared Function TerminateProcess(hProcess As UInteger, exitCode As Integer) As Boolean
+    End Function
+
+    <DllImport("ntdll.dll")>
+    Private Shared Function NtQueryInformationProcess(hProcess As IntPtr, processInformationClass As Integer, ByRef processBasicInformation As PROCESS_BASIC_INFORMATION, processInformationLength As UInteger, ByRef returnLength As UInteger) As Integer
+    End Function
+#End Region
+
+    ''' <summary>
+    ''' Locate peerflix.cmd (not currently needed)
+    ''' </summary>
+    ''' <returns>String path to peerflix.cmd</returns>
+    Private Function LocatePeerflix() As String
+        Dim paths() As String = Split(Environment.GetEnvironmentVariable("PATH"), ";")
+        Dim messageOutput As String = ""
+        Dim fileInPath As String = "peerflix.cmd"
+        For Each pathItem In paths
+            If File.Exists(pathItem & "\" & fileInPath) Then
+                Return pathItem & "\" & fileInPath
+            End If
+        Next
+        Return String.Empty
+    End Function
+
+    Private Sub VideoPlayer_PlayStateChange(sender As Object, e As _WMPOCXEvents_PlayStateChangeEvent) Handles VideoPlayer.PlayStateChange
+        Select Case e.newState
+            Case 1 'Stop 
+                StopPeerflix()
+            Case 2 'Paused
+            Case 3 'Play
+        End Select
+    End Sub
+
+    ''' <summary>
+    ''' Closes the form, shuts down the peerflix/nodejs service
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        StopPeerflix()
+    End Sub
+
+    ''' <summary>
+    ''' KeyDown event for SearchText, used to search on Keys.Enter
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub SearchTextBox_KeyDown(sender As Object, e As KeyEventArgs) Handles SearchTextBox.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            e.SuppressKeyPress = True
+            e.Handled = True
+            SearchButton.PerformClick()
+        End If
+    End Sub
+
+End Class
